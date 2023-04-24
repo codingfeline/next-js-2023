b7e3b36e05da8eacd32b6f167eb6fe36f8fc6a27
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-04-24 22:26:51 +0100

    min-h-screen : extend bg color when page scrolls

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ import Navbar from './Navbar'
 const Layout = ({ children }) => {
   return (
     <>
-      <div className="bg-purple-100 flex flex-col h-screen justify-between">
+      <div className="bg-purple-100 flex flex-col min-h-screen justify-between ">
         <Navbar />
         <div className="flex  flex-col mb-auto p-3 ">
           <main>{children}</main>
