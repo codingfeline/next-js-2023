366970c082aa69b1d965565a17d5fe2935b93b33
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-04-24 05:40:09 +0100

    added navbar

---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  const links = [
+    { to: '/', page: 'Home' },
+    { to: '/about', page: 'About' },
+    { to: '/contact', page: 'Contact' },
+    { to: '/signin', page: 'Sign In' },
+  ]
+
+  return (
+    <div>
+      <h2>navbar</h2>
+    </div>
+  )
+}
+
+export default Navbar
